8fe9002e900382eba25c44b2f05d50071670617b
Daniel Boakye <dartisan.inc@gmail.com>
2023-03-11 23:10:50 +0000

    feature: configuring infrastructure

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,15 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_HOST=database-1.cmji5u95g7f0.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432 
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=postgres
+export TYPEORM_DATABASE=postgres 
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+
+
+
+
+
+
